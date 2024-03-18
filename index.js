4ab49253cb41e88b20f33e2bf9737b9517ab2e73
eric <ericmaina11455@gmail.com>
2024-03-18 12:36:32 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+function writeCards(gifts,surprise){
+    const logMessages = [];
+    for(let i=0; i < gifts.length; i++){
+    const logMessage =`Thank you, ${gifts[i]}, for the wonderful ${surprise} gift!`;
+    logMessages.push(logMessage);
+    }
+
+    return logMessages;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function countDown(countD){
+//let countup = 10;
+while (countD >= 0) {
+  console.log(countD--);
+}
+}
+countDown(9);
